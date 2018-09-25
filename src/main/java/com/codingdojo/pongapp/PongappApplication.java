789000f18ba944c442f24baee744faff07371b70
Skyler Daché <skyberskyman@gmail.com>
2018-09-25 10:49:37 -0400

    addid initial spring files

---
+++ src/main/java/com/codingdojo/pongapp/PongappApplication.java
@@ -0,0 +1,12 @@
+package com.codingdojo.pongapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongappApplication.class, args);
+	}
+}
